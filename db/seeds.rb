77b093a9eced01b1506015a2cc81dcddb7e32f8c
Ian Earley <iearley88@gmail.com>
2017-10-09 12:08:42 -0600

    testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+User.create!(
+    email: "admin@test.com",
+    password: "asdfasdf",
+    password_confirmation: "asdfasdf",
+    name: "Admin User",
+    roles: "site_admin"
+    )
+puts "1 Admin User Created."
+
 3.times do |topic|
     Topic.create!(
         title: "Topic #{topic}"
